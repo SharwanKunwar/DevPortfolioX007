d9e76794f315d37e013a5eb4de24b5a2d453d098
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-14 16:21:07 +0545

    updated msg for phone user and laptop users

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,19 @@ import Navbar from './Components/Header/Navbar'
 import { Outlet } from 'react-router'
 
 function App() {
-  useEffect(()=>{
-    setTimeout(()=>{
-      alert("Open this site in laptop or pc for better experience");
-    },3000);
-  },[])
+  useEffect(() => {
+  const isMobileDevice = /android|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(
+    navigator.userAgent.toLowerCase()
+  );
+  const isSmallScreen = window.innerWidth <= 768;
+
+  if (isMobileDevice || isSmallScreen) {
+    setTimeout(() => {
+      alert("Open this site on a laptop or PC for a better experience.");
+    }, 3000);
+  }
+}, []);
+
   
   return (
   
