207847b9d52218cc4bbbfc422a4caa7c0258de79
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-14 16:13:38 +0545

    udpated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { useEffect } from 'react'
 import './App.css'
 import Navbar from './Components/Header/Navbar'
 import { Outlet } from 'react-router'
 
 function App() {
+  useEffect(()=>{
+    alert("Open this site in laptop or pc for better experience");
+  },[])
   
   return (
   
